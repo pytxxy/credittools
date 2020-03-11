b40516413421a1dc0dd95ed7567a2c0bc653ecde
caifh <cfh0081@163.com>
2020-03-11 10:50:26 +0800

    1.增加规避镜像服务器更新延迟的问题说明；

--- update_pypi.py
+++ update_pypi.py
@@ -34,6 +34,9 @@ https://packaging.python.org/tutorials/packaging-projects/
 使用样例:
 update_pypi.bat -u --vername 0.0.9b1.dev1 -b -t --upload -c --branch master D:\work\bc_dock_util
 
+从正式地址安装模块样例（用于规避使用镜像服务器升级更新不及时的情况）
+pip3 install --index-url https://pypi.org/simple/ your-package
+
 从测试地址安装模块样例
 pip3 install --index-url https://test.pypi.org/simple/ your-package
 pip3 install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple your-package
