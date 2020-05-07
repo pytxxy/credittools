17754b53bef9db70a216f78eed49726819adce22
caifh <cfh0081@163.com>
2020-05-07 20:02:54 +0800

    1.调整测试参数，进行实际验证。

--- protect_android_app.py
+++ protect_android_app.py
@@ -327,7 +327,7 @@ def protect(ip, user_name, key, secret, file_path, dst_path=None):
 
 
 if __name__ == '__main__':
-    test_args = '-i 10.192.76.12 -u pyzx -k c2a6ca06-0989-4ff5-82cd-10494fcc53eb -s 52a3724f-f45f-429a-932f-229bad482f54 D:\\auto_build\\pytxxy\\output\\pre\\20180816_161136\\3.4.8beta_p_456-1-20180816.apk'.split()
+    test_args = '-i 192.168.20.170 -u pyzx -k c2a6ca06-0989-4ff5-82cd-10494fcc53eb -s 52a3724f-f45f-429a-932f-229bad482f54 C:\\Users\\caifh.TXXY\\Desktop\\1.0.0beta_t_01-1-20200506.apk'.split()
     # test_args = None
     in_args = get_args(test_args)
     util.measure_time(main, in_args)
