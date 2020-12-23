efbda80bbca1abe2a074d44221b54c6b7d9e7f4c
caifh <caifh@txxy.com>
2020-12-23 20:52:59 +0800

    1.优化同步gitlab库到本地的业务逻辑。

--- sync_gitlab_to_local.py
+++ sync_gitlab_to_local.py
@@ -12,16 +12,14 @@ import creditutils.git_util as git_util
 import sync_git
 
 # 设计思路说明：
-# 1.先遍历老的gitlab库，将所有的库罗列出来；
-# 2.依次对所有老库所有分支进行如下操作：
-# (1)下载下来指定分支并更新(git pull --all)；
-# (2)调整remote地址，分别调用“git remote rm origin” 和 “git remote add origin 你的新远程仓库地址"(如 git remote add origin git@192.168.20.202:frontend/pk.git)进行更新；
-# (3)将代码同步到新的远程，调用命令为“git push --all origin”；
-# (4)中间有任何一个库操作失败，则记录下来，继续对下一个进行操作；
-# 3.特别说明，后续将只支持v4版本gitlab的同步，不再支持v3版本；（API V3 was unsupported from GitLab 9.5, released on August 22, 2017. API v3 was removed in GitLab 11.0.）
+# 1.先遍历gitlab库，将所有的库罗列出来；
+# 2.依次对所有库所有分支进行如下操作：
+# (1)下载下来指定分支并更新；
+# (2)中间有任何一个库操作失败，则记录下来，继续对下一个进行操作；
 
 class ApiLabel:
     v4 = 'v4'
+
 
 class DataLabel:
     id = 'id'
@@ -30,47 +28,6 @@ class DataLabel:
     ssh_url_to_repo = 'ssh_url_to_repo'
     path_with_namespace = 'path_with_namespace'
 
-def clean_history(repo_path, project):
-    temp_name = 'middle_temp_butt_none'
-    master_branch = None
-    try:
-        # 先取消分支保护
-        branches = project.branches.list()
-        master_branch = None
-        master_tag = 'master'
-        for branch in branches:
-            if master_tag == branch.name:
-                master_branch = branch
-
-            branch.unprotect()
-
-        # 一种获取分支方式
-        # master_branch = project.branches.get('master')
-
-        # 先更新git仓库信息
-        repo = git.Repo(repo_path)
-
-        # 先获取本地分支信息
-        local_branches = repo.branches
-        local_map = dict()
-        for item in local_branches:
-            local_map[item.name] = item
-
-        # 如果本地分支没有相应的远程分支，则创建，如果有则更新覆盖。
-        for k in local_map:
-            if k != repo.active_branch.name:
-                repo.git.checkout(k)
-
-            repo.git.checkout(temp_name, orphan=True)
-            repo.git.add(all=True)
-            repo.git.commit('Initial commit.', a=True, m=True)
-            repo.git.branch(k, d=True)
-            repo.git.branch(k, m=True)
-            repo.git.push('origin', k, f=True)
-    finally:
-        # 最后恢复分支保护
-        if master_branch:
-            master_branch.protect()
 
 class Manager:
     def __init__(self, args):
@@ -80,64 +37,64 @@ class Manager:
         #         pprint.pprint(vars(self))
 
         self.git_root = os.path.abspath(self.git_root)
+        self.valid_local_paths = []
+        self.invalid_local_paths = []
 
     def process(self):
-        self.sync_project()
-
-    def sync_project(self):
-        src_items = self.get_projects_sync_item(self.src, self.token)
-        other_info = dict()
-        # cnt_butt = 4
-        # cnt_index = 0
-        for k, v in src_items.items():
-            path = v.path
-            path_with_namespace = v.path_with_namespace
-            if not path_with_namespace.endswith(path):
-                print(f'{path_with_namespace} not endswith {path}!')
-                other_info[k] = v
-                continue
-
-            if path == path_with_namespace:
-                namespace = None
-                prj_path = self.git_root
-            else:
-                namespace = path_with_namespace[0:-len(path)]
-                prj_path = file_util.normalpath(os.path.join(self.git_root, namespace))
+        op = gitlab.Gitlab(self.src, private_token=self.token)
+        projects = op.projects.list(all=True)
+        for item in projects:
+            # cnt_butt = 4
+            # cnt_index = 0
             
-            prj_git_path = file_util.normalpath(os.path.join(self.git_root, path_with_namespace))
-            code_url = v.ssh_url_to_repo
-
-            # 只有本地不存在相关目录才从服务器端同步
-            if not os.path.isdir(prj_git_path):
-                print(f'to process {path_with_namespace}.')
-                try:
-                    if self.to_clean_history:
-                        # 先删除远程服务器上的tag信息，本地clone的时候就不会存在该信息
-                        if v.tags:
-                            tags = v.tags.list()
-                            for tag in tags:
-                                tag.delete()
-
-                    self.checkout(prj_path, path, code_url)
-                    sync_git.Manager.sync_repo(prj_git_path)
-                    
-                    if self.to_clean_history:
-                        clean_history(prj_git_path, v)
-
-                    print(f'processed {path_with_namespace}.')
-                except gitlab.exceptions.GitlabDeleteError as e:
-                    print(f'error: {str(e)}')
-            else:
-                print(f'{path_with_namespace} already exists.')
-
+            self.sync_project(item)
+            
             # cnt_index += 1
             # if cnt_index >= cnt_butt:
             #     break
 
-        print('need manual operation items:')
-        pprint.pprint(other_info)
+        # 清理本地有而服务器端已不存在的库
 
-    def checkout(self, prj_path, path, code_url, branch=None):
+    def sync_project(self, project):
+        path = project.path
+        path_with_namespace = project.path_with_namespace
+        if not path_with_namespace.endswith(path):
+            raise Exception(f'{path_with_namespace} not endswith {path}!')
+
+        if path == path_with_namespace:
+            namespace = None
+            prj_path = self.git_root
+        else:
+            namespace = path_with_namespace[0:-len(path)]
+            prj_path = file_util.normalpath(os.path.join(self.git_root, namespace))
+        
+        prj_git_path = file_util.normalpath(os.path.join(self.git_root, path_with_namespace))
+        code_url = project.ssh_url_to_repo
+
+        if self.to_reprocess:
+            if os.path.isdir(prj_git_path):
+                shutil.rmtree(prj_git_path)
+
+        # 将从服务器端checkout的目录添加到有效列表
+        self.valid_local_paths.append(prj_git_path)
+
+        print(f'to process {path_with_namespace}.')
+        self.checkout(prj_path, path, code_url)
+        sync_git.Manager.sync_repo(prj_git_path)
+        print(f'processed {path_with_namespace}.')
+
+    def collect_invalid_repo_recursive(self, repo_path):
+        file_list = os.listdir(repo_path)
+        for filename in file_list:
+            temp_file_path = os.path.join(repo_path, filename)
+            if os.path.isdir(temp_file_path):
+                if git_util.is_repository(temp_file_path):
+                    if temp_file_path not in self.valid_local_paths:
+                        self.invalid_local_paths.append(temp_file_path)
+                else:
+                    self.collect_invalid_repo_recursive(temp_file_path)
+
+    def checkout(self, prj_path, path, code_url):
         prj_git_root = os.path.join(prj_path, path)
         if not os.path.isdir(prj_path):
             os.makedirs(prj_path)
@@ -152,38 +109,17 @@ class Manager:
                         break
 
                     if remote_url != code_url:
-                        shutil.rmtree(prj_git_root, ignore_errors=False)
+                        shutil.rmtree(prj_git_root)
                     else:
                         return
                 else:
-                    shutil.rmtree(prj_git_root, ignore_errors=False)
+                    shutil.rmtree(prj_git_root)
             
         # git_util.clone(code_url, prj_path, branch=branch)
         try:
             git.Repo.clone_from(code_url, prj_git_root)
         except git.exc.GitCommandError as e:
             print(str(e))
-
-    def get_projects_sync_item(self, target, token):
-        results = dict()
-        src_items = self.get_projects_v4(target, token)
-        for src_item in src_items:
-            results[src_item.id] = src_item
-        
-        # print(results)
-        return results
-    
-    def get_projects_v4(self, target, token):
-        op = gitlab.Gitlab(target, private_token=token)
-        projects = op.projects.list(all=True)
-        # cnt = 0
-        # for project in projects:
-        #     print(project)
-        #     print(project.ssh_url_to_repo, project.name, project.id)
-        #     cnt += 1
-
-        # print(f'count is {cnt}')
-        return projects
 
 
 def main(args):
@@ -197,7 +133,7 @@ def get_args(src_args=None):
     parser.add_argument('src', metavar='src', help='source gitlab server')
     parser.add_argument('token', metavar='token', help='source gitlab server token')
     parser.add_argument('git_root', metavar='git_root', help='git root directory')
-    parser.add_argument('--clean-history', dest='to_clean_history', action='store_true', default=False, help='indicate to clean history and synchronise to remote')
+    parser.add_argument('--reprocess', dest='to_reprocess', action='store_true', default=False, help='indicate to reprocess the existing local project')
     
     # parser.print_help()
 
