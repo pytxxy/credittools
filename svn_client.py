e1ae4e5668547470c0b8da03044f65c57f97f7a6
caifh <caifh@txxy.com>
2021-03-22 17:57:09 +0800

    1.增加说明文字信息。

--- svn_client.py
+++ svn_client.py
@@ -2,7 +2,22 @@ import argparse
 import traceback
 import rpyc
 
-
+''' 
+1.先安装依赖
+ pip install rpyc
+2.配置python脚本运行环境
+ 将svn_client.py所有文件夹放到PYTHONPATH环境变量(windows环境可以将相应路径写在一个.pth结尾的文件并放到python.exe对应目录，
+linux可设置PYTHONPATH环境变量，在/etc/profile文件末尾增加设置，样例：export PYTHONPATH=/home/work/develop/python/main3)
+3.使用说明
+具体使用样例如下：
+服务器端设置了token，要进行token验证：
+python -m svn_client --host 127.0.0.1 --port 9999 --token what --commit txxy
+服务器端没有设置token，无须token验证：
+切换到正常提交模式
+python -m svn_client --host 127.0.0.1 --port 9999 --commit txxy
+切换到禁止提交模式
+python -m svn_client --host 127.0.0.1 --port 9999 txxy
+'''
 class Manager:
     HEAD_NAME = 'HEAD'
     def __init__(self, args):
