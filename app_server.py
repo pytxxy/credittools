088367baaaf26bac858d2b852fb8d038dc4c8c2e
caifh <caifh@txxy.com>
2021-04-20 16:14:33 +0800

    1.优化日志记录格式。

--- app_server.py
+++ app_server.py
@@ -58,7 +58,7 @@ class LocalLogger:
             logger.setLevel(level = logging.INFO)
             handler = logging.FileHandler(log_path)
             handler.setLevel(logging.INFO)
-            formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s:%(message)s')
+            formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s: %(message)s')
             handler.setFormatter(formatter)
             logger.addHandler(handler)
 
